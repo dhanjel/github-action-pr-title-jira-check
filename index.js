37406bd7d78341d0ffd335041a4569867feae293
Daniel Jansson <dhanjel@gmail.com>
2021-12-27 14:15:45 +0100

    Escape regex

--- index.js
+++ index.js
@@ -30,17 +30,17 @@ async function run() {
             pull_number: github.context.payload.pull_request.number
         });
         const pullRequestTitle = pullRequest.title;
-        core.info("Pull Request title: " + pullRequestTitle);
+        core.info('Pull Request title: ' + pullRequestTitle);
 
         // Verify that title contains a valid Jira key
-        var issueNumber = new RegExp(jiraProjectKey + "\-(\d+)\s.*", "g").exec(pullRequestTitle) || [""];
+        var issueNumber = new RegExp(jiraProjectKey + '-(\\d+)\\s.*', "g").exec(pullRequestTitle) || [""];
         if (issueNumber[0].length === 0) {
             core.setFailed('Could not find a valid Jira Issue number on the pull request title.');
             return;
         }
 
-        var issueKey = jiraProjectKey + "-" + issueNumber[0];
-        core.info("Jira Issue key: " + issueKey);
+        var issueKey = jiraProjectKey + '-' + issueNumber[0];
+        core.info('Jira Issue key: ' + issueKey);
 
         // Search for the issue in Jira
         var jira = new jiraClient({
@@ -57,7 +57,7 @@ async function run() {
         var jiraTitle = jiraIssueDetails.fields.summary;
         var jiraCompleteTitle = issueKey + " " + jiraTitle;
 
-        core.info("Jira Issue title: " + jiraCompleteTitle);
+        core.info('Jira Issue title: ' + jiraCompleteTitle);
 
         if (pullRequestTitle != jiraCompleteTitle) {
             core.setFailed('Expected the pull request title to be "' + jiraCompleteTitle + '", but it was "' + pullRequestTitle + '"');
