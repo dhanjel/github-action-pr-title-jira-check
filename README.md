83b49c65693a6522e2d9d75bafad569d4399f8b8
Daniel Jansson <dhanjel@gmail.com>
2021-12-27 10:02:40 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# github-action-pr-title-jira-check
+# Pull Request Title verifiration against Jira Github Action
+
+This action verifies that the pull request title is in the form `PROJECTKEY-XXX TITLE`, where `PROJECTKEY` is a valid Jira project key in your organization, `XXX` is the issue number, and `TITLE` is the summary of the Jira issue.
+
+## Inputs
+
+### `jira-host`
+
+**Required** The host url for your JIRA instance
+
+### `jira-username`
+
+**Required** JIRA Username
+
+### `jira-password`
+
+**Required** JIRA Password
+
+### `jira-project-key`
+
+**Required** JIRA Project Key
+
+### `github_token`
+
+Personal access token (PAT) used to fetch the repository. The PAT is configured
+with the local git config, which enables your scripts to run authenticated git
+commands. The post-job step removes the PAT.
+We recommend using a service account with the least permissions necessary.
+Also when generating a new PAT, select the least scopes necessary.
+
+## Outputs
+
+No outputs
+
+## Example usage
+
+`uses: actions/github-action-pr-title-jira-check 
+with:
+  jira-host: ''
+  jira-username: ''
+  jira-password: ''
+  jira-project-key: ''`
---
+++ index.js
@@ -0,0 +1,64 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const jiraClient = require('jira-client');
+
+try {
+
+    // Read input parameters
+    const jiraHost = core.getInput('jira-host', {required: true});
+    const jiraUsername = core.getInput('jira-username', {required: true});
+    const jiraPassword = core.getInput('jira-password', {required: true});
+    const jiraProjectKey = core.getInput('jira-project-key', {required: true});
+    const githubToken = core.getInput('github_token', {required: true});
+
+    // Verify invocation from pull request
+    const eventName = github.context.eventName;
+    if (eventName.indexOf('pull_request') < 0) {
+        core.setFailed('This action can only be used on pull requests.');
+        return;
+    }
+
+    // Get pull request title
+    const client = new github.GitHub(githubToken);
+    const {data: pullRequest} = await client.pulls.get({
+        owner,
+        repo,
+        pull_number: github.context.payload.pull_request.number
+      });
+    const pullRequestTitle = pullRequest.title;
+    core.setOutput("Pull Request title", pullRequestTitle);
+
+    // Verify that title contains a valid Jira key
+    var issueNumber = new RegExp("${jiraProjectKey}\-(\d+)", "g").exec(pullRequestTitle) || [""];
+    if (issueNumber[0].length === 0) {
+        core.setFailed('Could not find a valid Jira Issue number on the pull request title.');
+        return;
+    }
+
+    var issueKey = jiraProjectKey + "-" + issueNumber[0];
+    core.setOutput("Jira Issue key", issueKey);
+
+    // Search for the issue in Jira
+    var jira = new jiraClient({
+        protocol: 'https',
+        host: jiraHost,
+        username: jiraUsername,
+        password: jiraPassword,
+        apiVersion: '2',
+        strictSSL: true
+      });
+
+    var jiraIssueDetails = await jira.findIssue(issueKey);
+
+    var jiraTitle = jiraIssueDetails.fields.summary;
+    var jiraCompleteTitle = issueKey + " " + jiraTitle;
+
+    core.setOutput("Jira Issue title", jiraCompleteTitle);
+
+    if (pullRequestTitle != jiraCompleteTitle) {
+        core.setFailed('Expected the pull request title to be "' + jiraCompleteTitle + '", but it was "' + pullRequestTitle + '"');
+        return;
+    }
+} catch (error) {
+    core.setFailed(error.message);
+}
